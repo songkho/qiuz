ca1ae88314d143aae9e34a4cbff41589041613e8
songkho <songkho12@naver.com>
2020-06-17 13:36:56 +0900

    2020.06.17

--- app/src/main/java/com/idh/qiuz/FileExActivity.kt
+++ app/src/main/java/com/idh/qiuz/FileExActivity.kt
@@ -1,26 +1,37 @@
 package com.idh.qiuz
 
 import android.content.Context
+import android.content.pm.PackageManager
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Environment
 import android.text.TextUtils
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_file_ex.*
 import java.io.File
 import java.io.FileInputStream
 import java.io.FileNotFoundException
 import java.io.FileOutputStream
+import java.util.jar.Manifest
 
 class FileExActivity : AppCompatActivity() {
 
 
     val filename = "data.txt"
 
+
+    var granted = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_file_ex)
+
+        //외부저장소의 권한을 동적으로 체크하는 함수를 호출
+
+        checkPermission()
 
 
         savebutton.setOnClickListener {
@@ -29,22 +40,21 @@ class FileExActivity : AppCompatActivity() {
 
             val text = textField.text.toString()
 
-            when{   
+            when {
 
 
                 TextUtils.isEmpty(text) -> {
-                    Toast.makeText(applicationContext,"텍스트가 비어있습니다.", Toast.LENGTH_LONG).show()
+                    Toast.makeText(applicationContext, "텍스트가 비어있습니다.", Toast.LENGTH_LONG).show()
                 }
 
-                !isExternalStorageWritable()->{
-                    Toast.makeText(applicationContext,"외부 저장장치가 없습니다.", Toast.LENGTH_LONG).show()
+                !isExternalStorageWritable() -> {
+                    Toast.makeText(applicationContext, "외부 저장장치가 없습니다.", Toast.LENGTH_LONG).show()
                 }
 
                 else -> {
-                saveToInnerStroage(text , filename)
+                    saveToInnerStroage(text, filename)
+                }
             }
-            }
-
 
 
         }
@@ -57,24 +67,21 @@ class FileExActivity : AppCompatActivity() {
                 // textField의 텍스트를 불러온 텍스트로 설정한다.
                 textField.setText(loadFromInnerStorage(filename))
 
-            }catch (e: FileNotFoundException){
+            } catch (e: FileNotFoundException) {
                 //파일이 없는 경우 에러메세지 보여줌
 
-                Toast.makeText(applicationContext,"저장된 텍스트가 없습니다.", Toast.LENGTH_LONG).show()
+                Toast.makeText(applicationContext, "저장된 텍스트가 없습니다.", Toast.LENGTH_LONG).show()
 
             }
 
         }
 
 
-
-
-
     }
 
 
     //내부저장소파일의 텍스트를 저장한다.
-    fun saveToInnerStroage(text: String, filename:String){
+    fun saveToInnerStroage(text: String, filename: String) {
 
         //내부 저장소의 전달된 파일이름의 파일 출력 스트림을 가져온다.
 
@@ -93,7 +100,7 @@ class FileExActivity : AppCompatActivity() {
 
     //내부 저장소 파일의 텍스트를 불러온다
 
-    fun loadFromInnerStorage(filename : String):String{
+    fun loadFromInnerStorage(filename: String): String {
 
         // 내부저장소의 전달된 파일이름의 파일 입력 스트림을 가져온다.
 
@@ -107,9 +114,12 @@ class FileExActivity : AppCompatActivity() {
 
     fun isExternalStorageWritable(): Boolean {
 
-        when{
 
-            Environment.getExternalStorageState() == Environment.MEDIA_MOUNTED-> return true
+        //외부저장장치가 현재 사용가능 확인하는 함수 외부저장장치느 탈착이 가능한 겨우가 있다.
+
+        when {
+
+            Environment.getExternalStorageState() == Environment.MEDIA_MOUNTED -> return true
             else -> return false
 
         }
@@ -117,14 +127,14 @@ class FileExActivity : AppCompatActivity() {
     }
 
 
-    fun getAppDataFileFromExternalStorage(filename: String):File {
+    fun getAppDataFileFromExternalStorage(filename: String): File {
 
 
-        val dir = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT ){
+        val dir = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
 
             getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS)
 
-        }else{
+        } else {
             File(Environment.getExternalStorageDirectory().absolutePath + "/Documents")
         }
 
@@ -135,20 +145,103 @@ class FileExActivity : AppCompatActivity() {
     }
 
 
-    fun saveToExternalStrage(text: String, filename: String){
+    fun saveToExternalStrage(text: String, filename: String) {
 
         val fileOutputStream = FileOutputStream(getAppDataFileFromExternalStorage(filename))
         fileOutputStream.write(text.toByteArray())
         fileOutputStream.close()
 
 
-
     }
 
-    fun loadFromExternalStorage(filename: String):String{
+    fun loadFromExternalStorage(filename: String): String {
         return FileInputStream(getAppDataFileFromExternalStorage(filename)).reader().readText()
     }
 
+
+    val MY_PERMISSION_REQUEST = 999
+    fun checkPermission() {
+        val permissionCheck = ContextCompat.checkSelfPermission(
+            this@FileExActivity,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+        )
+
+        when {
+            permissionCheck != PackageManager.PERMISSION_GRANTED -> {
+
+                ActivityCompat.requestPermissions(
+
+                    this@FileExActivity,
+                    arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
+                    MY_PERMISSION_REQUEST
+
+
+                )
+
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<out String>, grantResults: IntArray
+    ) {
+
+
+        when (requestCode) {
+            MY_PERMISSION_REQUEST -> {
+                when {
+                    grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED -> {
+                        granted = true
+                    }
+                    else -> {
+                        granted = false
+                    }
+                }
+            }
+        }
+
+
+    }
+
+
+    fun saveToExternalCustomDirectory(text: String, filepath: String = "/sdcard/data.txt"){
+        when{
+
+
+            granted -> {
+
+                val fileOutputStream = FileOutputStream(File(filepath))
+
+                fileOutputStream.write(text.toByteArray())
+                fileOutputStream.close()
+
+            }else->{
+            Toast.makeText(applicationContext,"권한이 없습니다.", Toast.LENGTH_LONG).show()
+        }
+
+        }
+    }
+
+    //임의으 경로에 파일에서 데이터를 읽는 함수
+
+    fun loadFromExternalCustomDirectory(filepath: String = "/sdcard/data.txt"): String{
+
+        when{
+
+            //권한이 있는 경우
+
+            granted ->{
+                return FileInputStream(File(filepath)).reader().readText()
+            }
+
+            else->{
+                Toast.makeText(applicationContext,"권한이 없습니다.", Toast.LENGTH_LONG).show()
+                return ""
+            }
+
+        }
+
+    }
 
 
 
